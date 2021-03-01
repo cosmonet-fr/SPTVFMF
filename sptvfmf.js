79d32aa73e5280e17d30336f8158e75dc2e987cf
cosmonet-fr <60877684+cosmonet-fr@users.noreply.github.com>
2021-03-01 12:42:29 +0100

    a little better

--- sptvfmf.js
+++ sptvfmf.js
@@ -2,8 +2,6 @@ const chaine = "1 037,25 € Hors champ d'application de la TVA";
 
 const chaine_tronque = chaine.split("€");
 
-const chaine_tronque_espace = chaine_tronque[0].replace(' ', '');
-
-const nombre = parseFloat(chaine_tronque_espace.replace(',', '.'));
+const nombre = parseFloat(chaine_tronque[0].replace(' ', '').replace(',', '.'));
 
 console.log(nombre);
