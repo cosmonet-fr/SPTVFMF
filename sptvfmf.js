bb364a5dcb1b283b33794eee883d9344c9f05c75
cosmonet-fr <60877684+cosmonet-fr@users.noreply.github.com>
2021-03-04 15:04:47 +0100

    Edit function

--- sptvfmf.js
+++ sptvfmf.js
@@ -1,10 +1,8 @@
 const stringToNumber = (string, splittedCharacter) => {
   
   const truncatedString = string.split(splittedCharacter);
-  const number = parseFloat(truncatedString[0].replace(' ', '').replace(',', '.'));
-  
-  console.log(number);
-  return number;
+  return parseFloat(truncatedString[0].replace(' ', '').replace(',', '.'));
   
 }
-stringToNumber("1 037,25 € Hors champ d'application de la TVA", "€");
+const number = stringToNumber("1 037,25 € Hors champ d'application de la TVA", "€");
+console.log(number);
